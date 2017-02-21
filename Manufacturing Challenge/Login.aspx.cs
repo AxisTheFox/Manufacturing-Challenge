6a809e61975d4c683566313944f16187b60c7d45
Braydon <foxden00@gmail.com>
2017-02-21 14:23:11 -0500

    Refactored and cleaned user login code.

--- Manufacturing Challenge/Login.aspx.cs
+++ Manufacturing Challenge/Login.aspx.cs
@@ -12,6 +12,9 @@ namespace Manufacturing_Challenge
 {
     public partial class Login : System.Web.UI.Page
     {
+        string passwordFromDatabase = "";
+        int userId = 0;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -22,6 +25,10 @@ namespace Manufacturing_Challenge
             if (NoEmptyFieldsExist())
             {
                 AttemptLogin();
+            }
+            else
+            {
+                ShowEmptyFieldsMessage();
             }
         }
 
@@ -34,32 +41,18 @@ namespace Manufacturing_Challenge
         {
             try
             {
-                LogInUser();
+                AttemptUserLogin();
             }
             catch
             {
-                ShowErrorText();
+                ShowLoginErrorMessage();
             }
         }
 
-        private void LogInUser()
+        private void AttemptUserLogin()
         {
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["gamedb"].ConnectionString);
-            string qry = "SELECT ID, Password FROM [User] WHERE Email=@e;";
-            SqlCommand cmd = new SqlCommand(qry, conn);
-            cmd.Parameters.AddWithValue("@e", Email.Text);
-            string passwordFromDatabase = "";
-            int userId = 0;
-            conn.Open();
-            SqlDataReader rdr = cmd.ExecuteReader();
-            while (rdr.Read())
-            {
-                passwordFromDatabase = rdr["Password"].ToString();
-                userId = Int32.Parse(rdr["ID"].ToString());
-            }
-            rdr.Close();
-            conn.Close();
             string passwordFromUser = Password.Text;
+            ObtainPasswordFromDatabaseUsingEmail();
             if (passwordFromUser.Equals(passwordFromDatabase))
             {
                 Session["UserId"] = userId;
@@ -67,11 +60,43 @@ namespace Manufacturing_Challenge
             }
             else
             {
-                LoginFailedMessage.Text = "The Email or password you entered doesn't match our records.";
+                ShowInvalidLoginMessage();
             }
         }
 
-        private void ShowErrorText()
+        private void ObtainPasswordFromDatabaseUsingEmail()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["gamedb"].ConnectionString);
+            string qry = "SELECT ID, Password FROM [User] WHERE Email=@e;";
+            SqlCommand cmd = new SqlCommand(qry, conn);
+            cmd.Parameters.AddWithValue("@e", Email.Text);
+            conn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GetAccountDataFromReader(rdr);
+            rdr.Close();
+            conn.Close();
+        }
+
+        private void GetAccountDataFromReader(SqlDataReader rdr)
+        {
+            while (rdr.Read())
+            {
+                passwordFromDatabase = rdr["Password"].ToString();
+                userId = Int32.Parse(rdr["ID"].ToString());
+            }
+        }
+
+        private void ShowEmptyFieldsMessage()
+        {
+            LoginFailedMessage.Text = "We need your email and password to log you in.";
+        }
+
+        private void ShowInvalidLoginMessage()
+        {
+            LoginFailedMessage.Text = "The Email or password you entered doesn't match our records.";
+        }
+
+        private void ShowLoginErrorMessage()
         {
             LoginFailedMessage.Text = "Something went wrong, and we weren't able to log you in. Try again in a minute or so.";
         }
