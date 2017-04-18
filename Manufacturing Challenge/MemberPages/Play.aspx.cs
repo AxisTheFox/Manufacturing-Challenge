ac3d7af4e8d739bac2970490aebe46a07e63af3d
crecords <crecords@bsu.edu>
2017-04-18 14:55:10 -0400

    Re-added presentation of random scenarios, not just the last one. Made a checker in AdminPages/AddScenario that checks if fields are empty. Added an AssetsBar on Play page.

--- Manufacturing Challenge/MemberPages/Play.aspx.cs
+++ Manufacturing Challenge/MemberPages/Play.aspx.cs
@@ -4,28 +4,64 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace Manufacturing_Challenge.MemberPages
 {
     public partial class Play : System.Web.UI.Page
     {
         int userId;
+        int userStation;
         string userFirstName;
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["gamedb"].ConnectionString);
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["userId"] != null)
             {
                 userFirstName = Session["userFirstName"].ToString();
                 userId = (int)Session["userId"];
+                userStation = getStation(userId);
                 if (!IsPostBack)
                 {
                     //Do stuff? Like show board maybe?
+                    showAssets();
                 }
             }
             else
             {
                 Response.Redirect("/Login.aspx");
             }
+        }
+
+        private int getStation(int userId)
+        {
+            int station = 0;
+            conn.Open();
+            string qry = "select CurrentStation from [User] where ID = " + userId;
+            SqlCommand cmd = new SqlCommand(qry, conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                station = (int)rdr["CurrentStation"];
+                Session["userStation"] = station;
+            }
+            conn.Close();
+            return station;
+        }
+
+        private void showAssets()
+        {
+            /*conn.Open();
+            string qry = "select assetsMoney, assetsProducts, assetsParts, assetsEmployees, assetsCustomers from [User] where ID = " + userId;
+            SqlCommand cmd = new SqlCommand(qry, conn);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            assetsGridView.DataSource = dt;
+            assetsGridView.DataBind();*/
         }
 
         protected void btnAnswerScenario_Click(object sender, EventArgs e)
--- Manufacturing Challenge/MemberPages/Play.aspx.designer.cs
+++ Manufacturing Challenge/MemberPages/Play.aspx.designer.cs
@@ -20,5 +20,23 @@ namespace Manufacturing_Challenge.MemberPages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnAnswerScenario;
+        
+        /// <summary>
+        /// assetsGridView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView assetsGridView;
+        
+        /// <summary>
+        /// selectAssetsSQLDS control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource selectAssetsSQLDS;
     }
 }
